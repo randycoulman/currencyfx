5ee4dd2ab277fd58ba7d14a65c3b326405ac3667
Randy Coulman <rcoulman@gmail.com>
2015-06-02 22:05:52 -0700

    Flesh out the When and Then step definitions

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source "https://rubygems.org"
 group :test do
   gem "minitest", "~> 5.0"
   gem "cucumber", "~> 2.0"
+  gem "rspec-expectations", "~> 3.2"
 end
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,25 +1,29 @@
 Given(/^the exchange rate for ([\d.]+) ([A-Z]{3}) is ([\d.]+) ([A-Z]{3})$/) do |source_amount, source_currency, dest_amount, dest_currency|
-  pending
+  #pending
 end
 
 Given(/^the following currencies exist:?$/) do |table|
   # table is a Cucumber::Core::Ast::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+  #pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I convert ([\d.]+) from ([A-Z]{3}) to ([A-Z]{3})$/) do |amount, source, target|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I convert ([\d.]+) from (\w{3}) to (\w{3})$/) do |amount, source, target|
+  @output = run_application(amount, source, target)
 end
 
-Then(/^I should get ([\d.]+) ([A-Z]{3})$/) do |amount, currency|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^I should get ([\d.]+) (\w{3})$/) do |amount, currency|
+  expect(@output).to match /#{amount} #{currency.upcase}/
 end
 
 When(/^I ask for a currency list$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @output = run_application("--list")
 end
 
 Then(/^I should see currencies and descriptions in this order:$/) do |table|
-  # table is a Cucumber::Core::Ast::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+  expected_output = table.raw[1..-1].map { |row| row.join("\\s+|\\s+") }.join("\\s+|\\n|\\s+")
+  expect(@output).to match /#{expected_output}/m
+end
+
+def run_application(*args)
+  %x(bin/currencyfx #{args.join(" ")})
 end
