de085b0c44889f5213d1b5022a3f9d7778da1c3e
Randy Coulman <rcoulman@gmail.com>
2015-06-14 00:04:00 -0700

    Implement the list formatter

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem "httparty", "~> 0.13.5"
+gem "terminal-table", "~> 1.4"
 
 group :development, :test do
   gem "rake", "~> 10.4"
--- lib/currencyfx/list_formatter.rb
+++ lib/currencyfx/list_formatter.rb
@@ -1,7 +1,13 @@
+require "stringio"
+require "terminal-table"
+
 module Currencyfx
   class ListFormatter
     def format(currency_list)
-      fail "Test-drive this!"
+      Terminal::Table.new do |t|
+        t.headings = %w[Code Description]
+        t.rows = currency_list.map { |c| [c.code, c.description] }
+      end
     end
   end
 end
